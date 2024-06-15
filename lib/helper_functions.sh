a3adc2a444d1a74f9a2c5c1a8fe1edaed4cab745
ogabriel <gabrieloliver8991@gmail>
2024-06-14 21:42:01 -0300

    add helper functions

---
+++ lib/helper_functions.sh
@@ -0,0 +1,46 @@
+error='\033[0;31m'
+warn='\033[0;33m'
+info='\033[0;32m'
+no_color='\033[0m'
+
+function Error() {
+    local message=${1:-"ERROR: something went wrong"}
+    >&2 echo -e "${error}ERROR: $no_color$message"
+    exit 1
+}
+
+function Warn() {
+    local message=${1:-"WARNING: something went wrong"}
+    >&2 echo -e "${warn}WARNING: $no_color$message"
+}
+
+function Info() {
+    local message=${1:-"INFO: something went wrong"}
+    echo -e "${info}INFO: $no_color$message"
+}
+
+function CheckPermissons() {
+    if ! [[ $EUID -ne 0 ]]; then
+        Error "This script must not be run as root nor with sudo"
+    fi
+}
+
+function ValidateFunctionParams() {
+    if [[ $2 -lt $1 ]]; then
+        if [[ $1 -eq 1 ]]; then
+            Error "At least $1 param is required for $3"
+        else
+            Error "At least $1 params are required for $3"
+        fi
+    fi
+}
+
+function ValidateExactFunctionParams() {
+    if [[ $2 -ne $1 ]]; then
+        if [[ $1 -eq 1 ]]; then
+            Error "Exactly $1 param is required for $3"
+        else
+            Error "Exactly $1 params are required for $3"
+        fi
+    fi
+}
