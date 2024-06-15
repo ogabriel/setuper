20e264725bcea8151edb56a639a1e5e860cf18f7
ogabriel <gabrieloliver8991@gmail>
2024-06-14 22:08:53 -0300

    add entrypoint

---
+++ lib/main.sh
@@ -0,0 +1,23 @@
+if [[ $# -eq 0 ]]; then
+    printf "A simple and complete configuration manager!
+
+Usage:
+    Create your config files in ~/.config/setupper/
+
+    More information in the repository: https://github.com/ogabriel/setupper
+
+Options:
+    install    Install everything configured in the config files
+    "
+else
+    readonly lib_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+    readonly config_dir=${XDG_CONFIG_HOME:-$HOME/.config}/setupper
+
+    if [[ $1 == 'debug' ]]; then
+        source $lib_dir/debug.sh
+    elif [[ $1 == 'install' ]]; then
+        source $lib_dir/install.sh
+    else
+        printf "Invalid option\n"
+    fi
+fi
