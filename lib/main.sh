5678f0dbb85521cb854130c1f8979657f3aa2a95
ogabriel <gabrieloliver8991@gmail>
2024-06-16 20:36:28 -0300

    remove debug from main

--- lib/main.sh
+++ lib/main.sh
@@ -13,9 +13,7 @@ else
     readonly lib_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
     readonly config_dir=${XDG_CONFIG_HOME:-$HOME/.config}/setuper
 
-    if [[ $1 == 'debug' ]]; then
-        source $lib_dir/debug.sh
-    elif [[ $1 == 'install' ]]; then
+    if [[ $1 == 'install' ]]; then
         source $lib_dir/install.sh
     else
         printf "Invalid option\n"
