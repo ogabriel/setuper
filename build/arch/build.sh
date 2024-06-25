aa0d400f0abc5064ac8fbc81fef3d81ca7aa4591
ogabriel <gabrieloliver8991@gmail>
2024-06-25 09:21:31 -0300

    add logs

--- build/arch/build.sh
+++ build/arch/build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+echo "Building package"
 pacman -Sy base-devel --noconfirm --needed
+
+echo "Entering build directory"
 cd /build
+
+echo "Setup user"
 useradd --no-create-home --shell=/bin/false build
+
+echo "Setting permissions"
 chown -R build:build /build
+
+echo "Setting permissions for everyone"
 chmod -R 777 /build
+
+echo "Build package"
 su build -c "makepkg"
