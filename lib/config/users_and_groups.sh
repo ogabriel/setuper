6bf244b8e3b9bbe5fd98bfb88557c2bed37b3343
ogabriel <gabrieloliver8991@gmail.com>
2024-08-10 17:55:24 -0300

    separate users and groups stuff

---
+++ lib/config/users_and_groups.sh
@@ -0,0 +1,24 @@
+function User() {
+    ValidateFunctionParams 1 $# $FUNCNAME
+
+    local user=$@
+
+    shift
+    while [[ $# -gt 0 ]]; do
+        if [[ $1 =~ --groups=[a-z]+ ]]; then
+            shift
+        elif [[ $1 =~ --shell=[a-z]+ ]]; then
+            shift
+        else
+            Error "Invalid flag $1 for $FUNCNAME"
+        fi
+    done
+
+    users+=("$user")
+}
+
+function Group() {
+    ValidateFunctionParams 1 $# $FUNCNAME
+
+    groups+=($1)
+}
