c31c6872fdc1e4617c3d4a9b970f6b6f49ace70e
ogabriel <gabrieloliver8991@gmail.com>
2024-08-10 17:58:06 -0300

    separeate handling of users_and_groups

--- lib/handler_functions.sh
+++ lib/handler_functions.sh
@@ -1,60 +1,6 @@
 source $lib_dir/handler/asdf.sh
 source $lib_dir/handler/packages.sh
-
-function HandleGroups() {
-    for group in ${groups[*]}; do
-        if ! getent group $group &>/dev/null; then
-            Info "Creating group $group"
-            sudo groupadd $group
-        fi
-    done
-}
-
-function HandleUsers() {
-    for ((i = 0; i < ${#users[@]}; i++)); do
-        local user_config
-        readarray -d ' ' user_config <<<"${users[$i]}"
-
-        for ((j = 0; j < ${#user_config[@]}; j++)); do
-            if [[ ${user_config[j]} =~ --groups=.+ ]]; then
-                local groups=${user_config[j]#--groups=}
-                groups=${groups//[[:space:]]/}
-                groups=(${groups//,/ })
-            elif [[ ${user_config[j]} =~ --shell=.+ ]]; then
-                local shell=${user_config[j]#--shell=}
-                shell=${shell//[[:space:]]/}
-            else
-                local user=${user_config[j]}
-            fi
-        done
-
-        if ! id -u $user &>/dev/null; then
-            Info "Creating user $user"
-            sudo useradd $user
-        fi
-
-        if [[ ${groups} ]]; then
-            for group in ${groups_by_user[$user]}; do
-                if ! getent group $group &>/dev/null; then
-                    Info "Creating group $group for user $user"
-                    sudo groupadd $group
-                fi
-
-                if ! id $user | grep $group &>/dev/null; then
-                    Info "Adding user $user to group $group"
-                    sudo usermod -aG $group $user
-                fi
-            done
-        fi
-
-        if [[ $shell ]]; then
-            if ! getent passwd $user | cut -d : -f 7 | grep $shell &>/dev/null; then
-                Info "Changing shell of user $user to $shell"
-                sudo usermod -s $(which $shell) $user
-            fi
-        fi
-    done
-}
+source $lib_dir/handler/users_and_groups.sh
 
 function HandleFlatpakPackages() {
     if [[ ${#flatpak_packages[*]} -gt 0 ]]; then
